8feb00bab18d68494839e07d9548b30d0f6c6fe6
Sojib Ali <sojib.ali@hotmail.com>
2023-11-04 18:57:38 +0600

    chapter - 1 done

--- Chapter-1/app.js
+++ Chapter-1/app.js
@@ -1,6 +1,25 @@
-const n = 5;
-for(var i=0;i<n;i++){
-   if(i%2==0){
-    console.log("print=", i);
-   }
+// const n = 5;
+// for(var i=0;i<n;i++){
+//    if(i%2==0){
+//     console.log("print=", i);
+//    }
+// }
+
+// var n=7;
+/*function b(n){
+    if(n==0 || n==-1){
+        return 0;
+    }
+    else if(n%2==1){
+        console.log(n);
+        b(n-2);
+    }
+    else{
+        b(n-1);
+    } 
 }
+
+b(12); */
+// Math.random();
+console.log (Math.round(49.49));
+
