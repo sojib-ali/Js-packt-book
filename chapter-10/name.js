b45ca86a4b14e9dba6730ccef20e6eaf5815204d
Sojib Ali <sojib.ali@hotmail.com>
2023-12-05 18:52:50 +0600

    chapter-10 is done

---
+++ chapter-10/name.js
@@ -0,0 +1,20 @@
+// let x= document.querySelector(".main");
+// console.log(x.hasChildNodes());
+// if(x.hasChildNodes()){
+//     let children=x.childNodes.length;
+//     console.log(children);
+
+//     // for(const node of children){
+//     //     console.log(children[node]);
+//     // }
+// }
+
+// console.log(document.getElementById("two"));
+
+let i=0;
+let p=0;
+while(i<=10){  
+    console.log(++i);
+
+    
+}
